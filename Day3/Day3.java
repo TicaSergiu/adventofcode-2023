288f5337375e000da3e9b5b3ac8798484cdc7150
jonglet <mykymoto24@gmail.com>
2023-12-04 10:27:20 +0200

    Day3

---
+++ Day3/Day3.java
@@ -0,0 +1,132 @@
+package Day3;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collector;
+
+public class Day3 {
+    static final Map<String, Integer> map = new HashMap<>();
+
+    public static void main(String[] args) throws IOException {
+        // part1();
+        part2();
+    }
+
+    static void part1() throws IOException {
+        List<String> lines = Files.readAllLines(Path.of("Day3/input.txt"));
+        int sum = 0;
+        for (int i = 0; i < lines.size(); i++) {
+            String line = lines.get(i);
+            for (int j = 0; j < line.length(); j++) {
+                if (Character.isDigit(line.charAt(j))) {
+                    int number = 0;
+                    boolean validNumber = false;
+                    if (Character.isDigit(line.charAt(j + 1)) && Character.isDigit(line.charAt(j + 2))) {
+                        validNumber = findNeighbour(lines, i, j, 3);
+                        number = Integer.parseInt(line.substring(j, j + 3));
+                        j += 2;
+                    } else if (Character.isDigit(line.charAt(j + 1))) {
+                        validNumber = findNeighbour(lines, i, j, 2);
+                        number = Integer.parseInt(line.substring(j, j + 2));
+                        j++;
+                    } else {
+                        number = line.charAt(j) - '0';
+                        validNumber = findNeighbour(lines, i, j, 1);
+                    }
+                    if (validNumber) {
+                        sum += number;
+                    }
+                }
+            }
+        }
+        System.out.println("Part 1 answer: " + sum);
+    }
+
+    private static boolean findNeighbour(List<String> lines, int lineNumber, int charLocation, int numSize) {
+        for (int k = 1; k > -2; k--) {
+            try {
+                String currentLine = lines.get(lineNumber - k);
+                for (int i = charLocation - 1; i <= charLocation + numSize; i++) {
+                    try {
+                        if (currentLine.charAt(i) != '.' && !Character.isDigit(currentLine.charAt(i))) {
+                            return true;
+                        }
+                    } catch (Exception e) {
+                    }
+                }
+            } catch (Exception e) {
+            }
+        }
+        return false;
+    }
+
+    static void part2() throws IOException {
+        List<String> lines = Files.readAllLines(Path.of("Day3/example.txt"));
+        int result = 0;
+        for (int i = 0; i < lines.size(); i++) {
+            String line = lines.get(i);
+            for (int j = 0; j < line.length(); j++) {
+                if (line.charAt(j) == '*') {
+                    List<Integer> nums = new ArrayList<>();
+                    final Pattern pattern = Pattern.compile("[\\d]{1,3}");
+                    Matcher matcher = null;
+                    try {
+                        String previous = lines.get(i - 1);
+                        if (Character.isDigit(previous.charAt(j - 1))) {
+                            if (Character.isDigit(previous.charAt(j))) {
+                            }
+
+                        }
+                    } catch (Exception e) {
+                    }
+                    matcher = pattern.matcher(line.substring(j - 3, j + 4));
+                    while (matcher.find()) {
+                        nums.add(Integer.parseInt(matcher.group(0)));
+                    }
+                    String next = lines.get(i + 1);
+
+                    if (nums.size() == 2) {
+                        result += nums.get(0) * nums.get(1);
+                    }
+                }
+            }
+        }
+        System.out.println("Part 2 answer: " + result);
+    }
+
+    private static Integer checkNumber(String line, int j) {
+        if (Character.isDigit(line.charAt(j)) && Character.isDigit(j + 1) && Character.isDigit(j + 2)) {
+            return Integer.parseInt(line.substring(j, j + 4));
+        }
+        if (Character.isDigit(line.charAt(j)) && Character.isDigit(j + 1)) {
+            return Integer.parseInt(line.substring(j, j + 3));
+        }
+        if (Character.isDigit(line.charAt(j))) {
+            return Integer.parseInt(line.substring(j, j + 2));
+        }
+        return 0;
+    }
+
+    private static Integer checkReverseNumber(String previousLine, int j) {
+        if (Character.isDigit(previousLine.charAt(j - 2)) && Character.isDigit(previousLine.charAt(j - 1))
+                && Character.isDigit(previousLine.charAt(j - 0))) {
+
+            return Integer.parseInt(previousLine.substring(j - 2, j - 0), 3);
+        }
+        if (Character.isDigit(previousLine.charAt(j - 1)) && Character.isDigit(previousLine.charAt(j - 0))) {
+            return Integer.parseInt(previousLine.substring(j - 2, j - 0), 2);
+        }
+        if (Character.isDigit(previousLine.charAt(j - 0))) {
+            return Integer.parseInt(previousLine.substring(j - 1, j - 0), 1);
+        }
+        return 0;
+    }
+}
